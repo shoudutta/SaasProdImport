1c95385ded8be2d66ceb1064c7addace18bde20e
Shounak Dutta <Shounak.Dutta@hitachiconsulting.com>
2021-01-27 02:54:15 +0530

    Adding unit tests.

--- UnitTestProject1/FormatServiceTest.cs
+++ UnitTestProject1/FormatServiceTest.cs
@@ -1,10 +1,68 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaasProdImport.Models;
+using SaasProdImport.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SaasProdImport.UnitTests
 {
-    class FormatServiceTest
+    [TestClass]
+    public class FormatServiceTest : TestBase
     {
+        private static FormatService formatService;
+        private static Format format;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            TestBase testBase = new TestBase();
+            formatService = new FormatService(TestBase.context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            format = new Format
+            {
+                FormatId = 0,
+                FormatName = "csv"
+            };
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            format = null;
+        }
+
+        [TestMethod]
+        public void GetFormats()
+        {
+            var dummyFormats = formatService.GetFormats();
+            Assert.IsNotNull(dummyFormats);
+            Assert.AreEqual(2, dummyFormats.Count);
+        }
+
+        [TestMethod]
+        public void GetFormatByName()
+        {
+            var jsonFormat = formatService.GetFormatByName("json");
+            Assert.IsNotNull(jsonFormat);
+
+            var csvFormat = formatService.GetFormatByName("csv");
+            Assert.IsNull(csvFormat);
+        }
+
+        [TestMethod]
+        public void AddFormat()
+        {
+            formatService.AddFormat(format.FormatName);
+            var dummyFormats = formatService.GetFormats();
+            Assert.IsNotNull(dummyFormats);
+            Assert.AreEqual(3, dummyFormats.Count);
+            Assert.IsFalse(dummyFormats.Contains(format));
+            Assert.IsTrue(dummyFormats.Exists(x => x.FormatName == format.FormatName));
+        }
     }
 }
--- UnitTestProject1/ProductServiceTest.cs
+++ UnitTestProject1/ProductServiceTest.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaasProdImport.Models;
+using SaasProdImport.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SaasProdImport.UnitTests
 {
-    class ProductServiceTest
+    [TestClass]
+    public class ProductServiceTest : TestBase
     {
+        private static ProductService productService;
+        private static Product product;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            TestBase testBase = new TestBase();
+            productService = new ProductService(TestBase.context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            product = new Product
+            {
+                ProductId = 0,
+                ProductConfiguration = "domain 1, domain 2",
+                ProductName = "Dummy product",
+                ProductTwitter = "Dummy twitter"
+            };
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            product = null;
+        }
+
+        [TestMethod]
+        public void GetProducts()
+        {
+            var dummyProducts = productService.GetProducts();
+            Assert.IsNotNull(dummyProducts);
+            Assert.AreEqual(0, dummyProducts.Count);
+        }
+
+        [TestMethod]
+        public void AddProduct()
+        {
+            product.ProductId = productService.GetNextId();
+            Assert.AreEqual(1, product.ProductId);
+
+            productService.AddProduct(product);
+            var dummyProducts = productService.GetProducts();
+            Assert.IsNotNull(dummyProducts);
+            Assert.AreEqual(1, dummyProducts.Count);
+            Assert.IsTrue(dummyProducts.Contains(product));
+        }
     }
 }
